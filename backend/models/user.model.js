f615553b5890dfa8851fcbbb22e6429fd3111912
RandilT <randiltharusha72@gmail.com>
2024-03-01 15:30:50 +0530

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    maxLength: 25,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    maxLength: 25,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female", "other"],
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
