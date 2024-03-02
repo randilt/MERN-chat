5bf65f5ab856e67a99eab04fdc24da109ec87f3d
RandilT <randiltharusha72@gmail.com>
2024-03-02 14:09:56 +0530

    create message model

---
+++ backend/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+); // timestamps: true adds createdAt and updatedAt fields to the schema
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
