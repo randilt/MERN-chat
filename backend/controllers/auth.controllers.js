dd8e4546eb687336ab752758f8e41182b1bef1e2
RandilT <randiltharusha72@gmail.com>
2024-03-02 10:08:45 +0530

    update username to userName

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -58,8 +58,8 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ userName: username });
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName: userName });
     const isPasswordCorrect = await bcrypt.compare(
       password,
       user.password || ""
