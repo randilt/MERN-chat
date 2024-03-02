b261457424855fdd74abe9d7e092822127850816
RandilT <randiltharusha72@gmail.com>
2024-03-02 21:59:47 +0530

    import cookieParser pkg

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,6 +12,7 @@ dotenv.config();
 app.use(express.json()); // for parsing application/json and setting up req.body
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use(cookieParser());
 
 // app.get("/", (req, res) => {
 //   {
